fbc5595f4a98023c46180fa36be15ce29effdac4
febo <febo@anza.xyz>
2025-06-18 00:38:00 +0100

    Add p-token feature

--- svm-feature-set/src/lib.rs
+++ svm-feature-set/src/lib.rs
@@ -37,6 +37,7 @@ pub struct SVMFeatureSet {
     pub formalize_loaded_transaction_data_size: bool,
     pub disable_zk_elgamal_proof_program: bool,
     pub reenable_zk_elgamal_proof_program: bool,
+    pub migrate_ptoken_to_spl_token_program: bool,
 }
 
 impl SVMFeatureSet {
@@ -79,6 +80,7 @@ impl SVMFeatureSet {
             formalize_loaded_transaction_data_size: true,
             disable_zk_elgamal_proof_program: true,
             reenable_zk_elgamal_proof_program: true,
+            migrate_ptoken_to_spl_token_program: true,
         }
     }
 }
